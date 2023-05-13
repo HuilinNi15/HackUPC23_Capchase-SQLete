988736ffe730b3c2d845b3021a877ab3189bdaa0
CPerezRuiz335 <carlosp335@gmail.com>
2023-05-13 19:18:41 +0200

    team

--- files/coordinate.lua
+++ files/coordinate.lua
@@ -18,23 +18,29 @@ end
 function bot_main(me)
 
 	if ticks == 0 then
-		for key, player in ipairs(me:visible()) do
+		for key, player in pairs(me:visible()) do
 			init_positions[player:id()] = player:pos()
+			print('ticks = 0', player:id(), player:pos(), player:type())
 		end
-		print('entro tick 0')
+		-- print('mi posicion', me:pos():x())
 		me:move(init_direction)
+		-- print('mi posicion', me:pos())
+
+
 	end
 
 	if ticks == 1 then
-		for key, player in pairs(me:visible()) do
+		for key, player in ipairs(me:visible()) do
+			print('ticks = 1', player:id(), player:pos(), player:type())
+			print()
 			for init_id, init_pos in pairs(init_positions) do
 				if player:id() == init_id then
-					print("m'he reconegut")
-					print(player:id())
 					local diff = player:pos():sub(init_pos)
 					local len = math.sqrt(diff:x()^2 + diff:y()^2)
 					local unit_x = diff:x() / len
 					local unit_y = diff:y() / len
+					print('entro seguindo for')
+					print(player:id())
 					print(abs_(init_direction:x() - unit_x))
 					if abs_(init_direction:x() - unit_x) <= tol and abs_(init_direction:y() - unit_y) <= tol then
 						table.insert(team_ids, init_id)
@@ -43,5 +49,10 @@ function bot_main(me)
 			end
 		end
 	end
+	print('printeo team')
+	for _, player in pairs(team_ids) do
+		print(player)
+	end
+
 	ticks = ticks + 1
 end
